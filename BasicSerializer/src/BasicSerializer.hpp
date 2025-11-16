376639ade65e639b3d749d87c9e850b7f3af5a21
SamHalvoe <46531812+SamHalvoe@users.noreply.github.com>
2025-11-16 21:30:27 +0100

    - some refactoring and cleanup

--- BasicSerializer/src/BasicSerializer.hpp
+++ BasicSerializer/src/BasicSerializer.hpp
@@ -64,8 +64,8 @@ namespace halvoe
     success = 0,
     writeOutOfRange,
     writeStringOutOfRange,
-    writeStringSizeOutOfRange,
-    writeStringIsNullptr
+    writeStringIsNullptr,
+    writeStringOutOfRangeOrNotZeroterminated
   };
 
   enum class DeserializerStatus : uint8_t
@@ -73,7 +73,6 @@ namespace halvoe
     success = 0,
     readOutOfRange,
     readStringOutOfRange,
-    readStringSizeOutOfRange,
     readStringOutIsNullptr,
     readStringOutOfMemory,
     stringAllocationFailed,
@@ -100,12 +99,12 @@ namespace halvoe
       {
         switch (in_code)
         {
-          case SerializerStatus::success:                   return "operation successful";
-          case SerializerStatus::writeOutOfRange:           return "write operation out of range";
-          case SerializerStatus::writeStringOutOfRange:     return "write string operation out of range";
-          case SerializerStatus::writeStringSizeOutOfRange: return "write string size operation out of range";
-          case SerializerStatus::writeStringIsNullptr:      return "write string string is nullptr";
-          default:                                          return "invalid SerializerStatus";
+          case SerializerStatus::success:                                  return "operation successful";
+          case SerializerStatus::writeOutOfRange:                          return "write operation out of range";
+          case SerializerStatus::writeStringOutOfRange:                    return "write string operation out of range";
+          case SerializerStatus::writeStringIsNullptr:                     return "write string string is nullptr";
+          case SerializerStatus::writeStringOutOfRangeOrNotZeroterminated: return "write string operation out of range or string is not zeroterminated";
+          default:                                                         return "invalid SerializerStatus";
         }
       }
 
@@ -116,7 +115,6 @@ namespace halvoe
           case DeserializerStatus::success:                  return "operation successful";
           case DeserializerStatus::readOutOfRange:           return "read operation out of range";
           case DeserializerStatus::readStringOutOfRange:     return "read string operation out of range";
-          case DeserializerStatus::readStringSizeOutOfRange: return "read string size operation out of range";
           case DeserializerStatus::readStringOutIsNullptr:   return "read string out_parameter is nullptr";
           case DeserializerStatus::readStringOutOfMemory:    return "read string out of memory";
           case DeserializerStatus::stringAllocationFailed:   return "string allocation failed";
@@ -281,10 +279,11 @@ namespace halvoe
       {
         static_assert(isSizeType<SizeType>(), "SizeType must be an unsigned int!");
         if (in_string == nullptr) { return error(SerializerStatus::writeStringIsNullptr); }
-        size_t size = strnlen(in_string, getBytesLeft());
-        if (m_cursor + sizeof(SizeType) + size > tc_bufferSize) { return error(SerializerStatus::writeStringOutOfRange); }
+        size_t maxStringSize = getBytesLeft() - sizeof(SizeType);
+        size_t size = strnlen(in_string, maxStringSize + 1);
+        if (size == maxStringSize + 1) { return error(SerializerStatus::writeStringOutOfRangeOrNotZeroterminated); }
         
-        if (write<SizeType>(size) != SerializerStatus::success) { return error(SerializerStatus::writeStringSizeOutOfRange); }
+        write<SizeType>(size); // we can ignore the return value here, because we already checked for out of range above
         std::memcpy(m_begin + m_cursor, in_string, size);
         m_cursor = m_cursor + size;
         return SerializerStatus::success;
@@ -297,7 +296,7 @@ namespace halvoe
         if (in_string == nullptr) { return error(SerializerStatus::writeStringIsNullptr); }
         if (m_cursor + sizeof(SizeType) + in_size > tc_bufferSize) { return error(SerializerStatus::writeStringOutOfRange); }
 
-        if (write<SizeType>(in_size) != SerializerStatus::success) { return error(SerializerStatus::writeStringSizeOutOfRange); }
+        write<SizeType>(in_size); // we can ignore the return value here, because we already checked for out of range above
         std::memcpy(m_begin + m_cursor, in_string, in_size);
         m_cursor = m_cursor + in_size;
         return SerializerStatus::success;
@@ -433,9 +432,8 @@ namespace halvoe
         if (out_string == nullptr) { return make_error(DeserializerStatus::readStringOutIsNullptr); }
         if (m_cursor + sizeof(SizeType) + in_maxStringSize > tc_bufferSize) { return make_error(DeserializerStatus::readStringOutOfRange); }
         
-        auto sizeElement = read<SizeType>();
-        if (not sizeElement.has_value()) { return make_error(DeserializerStatus::readStringSizeOutOfRange); }
-        const SizeType size = sizeElement.value() < in_maxStringSize ? sizeElement.value() : in_maxStringSize - 1;
+        SizeType size = read<SizeType>().value(); // we can retrieve the value right here, because we already checked for out of range above
+        size = size < in_maxStringSize ? size : in_maxStringSize - 1;
         
         std::memcpy(out_string, m_begin + m_cursor, size);
         out_string[size] = '\0';
@@ -455,9 +453,8 @@ namespace halvoe
         static_assert(isSizeType<SizeType>(), "Type must be an unsigned int!");
         if (m_cursor + sizeof(SizeType) + in_maxStringSize > tc_bufferSize) { return make_error(DeserializerStatus::readStringOutOfRange); }
         
-        auto sizeElement = read<SizeType>();
-        if (not sizeElement.has_value()) { return make_error(DeserializerStatus::readStringSizeOutOfRange); }
-        const SizeType size = sizeElement.value() < in_maxStringSize ? sizeElement.value() : in_maxStringSize;
+        SizeType size = read<SizeType>().value(); // we can retrieve the value right here, because we already checked for out of range above
+        size = size < in_maxStringSize ? size : in_maxStringSize - 1;
 
         StringFromBytes string(m_begin + m_cursor, size);
         if (not string) { return make_error(DeserializerStatus::readStringOutOfMemory); }
